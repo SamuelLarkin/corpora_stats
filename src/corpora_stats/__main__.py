e335d092bdb26b608b049a07995b45bef7f9d53e
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2024-07-30 12:21:02 -0400

    feat: added mean and sdev

--- src/corpora_stats/__main__.py
+++ src/corpora_stats/__main__.py
@@ -5,33 +5,71 @@ import sys
 from xopen import xopen
 from typing import Tuple
 from dataclasses import dataclass, field
+import dataclasses_json
 from dataclasses_json import dataclass_json
+from math import sqrt
 
 
-@dataclass_json
 @dataclass
-class Count:
-    count: int = 0
-    count_square: int = 0
+class Stats(dataclasses_json.DataClassJsonMixin):
+    n: int = 0
+    sum: int = 0
+    sum_square: int = 0
     min: int = sys.maxsize
     max: int = 0
 
     def update(self, x: int):
-        self.count += x
-        self.count_square += x * x
+        self.n += 1
+        self.sum += x
+        self.sum_square += x * x
         if x < self.min:
             self.min = x
         if x > self.max:
             self.max = x
+
+    @property
+    def mean(self) -> float:
+        return float(self.sum) / self.n if self.n >= 0 else 0.0
+
+    @property
+    def variance(self) -> float:
+        if self.n > 1:
+            return (float(self.sum_square) - float(self.sum * self.sum) / self.n) / (
+                self.n - 1
+            )
+        else:
+            return 0.0
+
+    @property
+    def sdev(self) -> float:
+        return sqrt(self.variance)
+
+    def to_dict(
+        self,
+        encode_json: bool = False,
+    ) -> dict[str, dataclasses_json.core.Json]:
+        """
+        override the method in order to add computable properties to JSON
+        https://github.com/lidatong/dataclasses-json/issues/176
+        """
+
+        # first call the parent method to get non-computable properties
+        data = super().to_dict(encode_json=encode_json)
+
+        # then manually set computable properties
+        data["mean"] = self.mean
+        data["sdev"] = self.sdev
+
+        return data
 
 
 @dataclass_json
 @dataclass
 class Document:
     filename: str
-    char_count: Count = field(default_factory=Count)
-    unicode_count: Count = field(default_factory=Count)
-    word_count: Count = field(default_factory=Count)
+    char_count: Stats = field(default_factory=Stats)
+    unicode_count: Stats = field(default_factory=Stats)
+    word_count: Stats = field(default_factory=Stats)
     line_count = 0
 
     def update(self, line: str):
@@ -52,7 +90,7 @@ class Document:
 def wc(
     files: Tuple[str],
 ):
-    line_per_doc = Count()
+    line_per_doc = Stats()
     for filename in files:
         with xopen(filename) as cin:
             doc = Document(filename)
