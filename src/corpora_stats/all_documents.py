76299ff1aee3cc8c97d883ab006746e86d0fefac
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2024-10-10 11:26:26 -0400

    feat: reorder overall stats

--- src/corpora_stats/all_documents.py
+++ src/corpora_stats/all_documents.py
@@ -11,10 +11,10 @@ class AllDocuments(dataclasses_json.DataClassJsonMixin):
     A class to handle the overall document statistics.
     """
 
-    line: Stats = field(default_factory=Stats)
     bytes: Stats = field(default_factory=Stats)
     char: Stats = field(default_factory=Stats)
     word: Stats = field(default_factory=Stats)
+    line: Stats = field(default_factory=Stats)
 
     def __iadd__(self, other: "Document") -> "AllDocuments":
         self.line.update(other.line)
